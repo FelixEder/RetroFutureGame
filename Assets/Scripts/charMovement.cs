187dd4dbd7d22c82f32c2f6d92c1dea187328f10
felixed <felixed@kth.se>
2016-06-30 18:29:57 +0200

    Added walking code for player object

--- Assets/Scripts/charMovement.cs
+++ Assets/Scripts/charMovement.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
+	//Fields
+	float speed = 1.0f;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		int move = Vector3(Input.getAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
 	}
 }

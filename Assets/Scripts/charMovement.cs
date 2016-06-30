2c7c51fd3826928e7df11eaf1652b3b8b148cc1f
felixed <felixed@kth.se>
2016-06-30 21:49:19 +0200

    Documentation added for CharMovement.cs

--- Assets/Scripts/charMovement.cs
+++ Assets/Scripts/charMovement.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class charMovement : MonoBehaviour {
+/**
+ * This class handles the movement for the player, including movement and jumps.
+ */
+public class CharMovement : MonoBehaviour {
 	//Fields
 	public float moveSpeed;
 	public float jumpSpeed;
@@ -11,13 +14,22 @@ public class charMovement : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update() {
+	void Update(){
+	}
+
+	/**
+	 * Fixed update executes with a set time interval and calculates all physics equations required.
+	 */
+	void FixedUpdate() {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			Jump ();
 		}
 		GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 	}
 
+	/**
+	 * When called, makes the player character jump a set height.
+	 */ 
 	void Jump() {
 		GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
 	}

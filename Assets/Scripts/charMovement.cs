62859e4412c7292b0c77d7a8497333ea3f45e7b8
Felix Eder <felix.eder@mail.com>
2016-06-30 22:38:05 +0200

    Reverted raycast to old jump method

--- Assets/Scripts/charMovement.cs
+++ Assets/Scripts/charMovement.cs
@@ -8,7 +8,6 @@ public class CharMovement : MonoBehaviour {
 	//Fields
 	public float moveSpeed;
 	public float jumpSpeed;
-	private bool isGrounded;
 
 	// Use this for initialization
 	void Start() {
@@ -22,19 +21,16 @@ public class CharMovement : MonoBehaviour {
 	 * Fixed update executes with a set time interval and calculates all physics equations required.
 	 */
 	void FixedUpdate() {
-		isGrounded = Physics.RayCast(transformation.position, transformation.up * -1,  m_GroundCastLength, 1 << LayerMask.NameToLayer ("Ground"));
-
-		if (Input.GetKeyDown(KeyCode.Space) && m_grounded) {
-			Jump ();
-		}
 		GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-	}	
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Jump();
+		}
+	}
 
 	/**
 	 * When called, makes the player character jump a set height.
 	 */ 
 	void Jump() {
 		GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
-		isGrounded = false;
 	}
 }

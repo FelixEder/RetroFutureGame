3043fcf185a78698a87339305db60bdfd3d1b2ab
felixed <felixed@kth.se>
2016-06-30 19:24:14 +0200

    Reworked the jump and movement code

--- Assets/Scripts/charMovement.cs
+++ Assets/Scripts/charMovement.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class charMovement : MonoBehaviour {
 	//Fields
-	float horizontalSpeed = 2.0f;
-	float verticalSpeed = 5.0f;
+	float speed = 2.0f;
+	float jumpSpeed = 10.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,11 +12,11 @@ public class charMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		var horizontalMove = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-		var verticalMove = new Vector3(0, Input.GetAxis("Vertical"), 0);
+		rigidbody2d.veloctiy = new Vector2 (Input.GetAxis ("Horizontal") * speed, Input.GetAxis ("Vertical") * speed);
+		if (Input.GetButton (JumpButton)) Jump ();
+	}
 
-		transform.position += horizontalMove * horizontalSpeed * Time.deltaTime;
-		transform.position += verticalMove * verticalSpeed * Time.deltaTime;
-	
+	void Jump() {
+		rigidbody2D.AddForce (Vector2.up * jumpSpeed);
 	}
 }

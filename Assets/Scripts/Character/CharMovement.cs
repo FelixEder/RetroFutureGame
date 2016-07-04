37ae6878a393352739e81d921103fbe09de05033
Felix Eder <felix.eder@mail.com>
2016-07-04 17:00:47 +0200

    Reworked movement script

--- Assets/Scripts/Character/CharMovement.cs
+++ Assets/Scripts/Character/CharMovement.cs
@@ -1,14 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
 
 //This class handles the movement for the player, including walking and jumps.
 public class CharMovement : MonoBehaviour {
 	//Fields
 	CharStatus status;
 	Rigidbody2D rigidBody2D;
-	public float moveSpeed, airMoveSpeed, maxMoveSpeed;
-	public float moveAcceleration;
+	public float moveSpeed, airSpeed, maxMoveSpeed, moveForce;
+	float axisH;
 
 	//Start is called once on initialization
 	void Start() {
@@ -16,32 +15,45 @@ public class CharMovement : MonoBehaviour {
 		rigidBody2D = GetComponent<Rigidbody2D> ();
 	}
 
-	//Update is called once per frame
-	void Update(){
-	}
-
 	//FixedUpdate executes with a set time interval and calculates all physics equations required.
 	void FixedUpdate() {
-		if (status.onLeftWall && Input.GetAxis("Horizontal") < 0) {
-			rigidBody2D.velocity = new Vector2(0, rigidBody2D.velocity.y);
-		} else if (status.onRightWall && Input.GetAxis ("Horizontal") > 0) {
-			rigidBody2D.velocity = new Vector2(0, rigidBody2D.velocity.y);
-		} else {
-			moveAcceleration = rigidBody2D.velocity.x * moveSpeed + Input.GetAxis("Horizontal");
-			if (moveAcceleration > maxMoveSpeed) {
-				moveAcceleration = maxMoveSpeed;
-			} else if(moveAcceleration < maxMoveSpeed * -1) {
-				moveAcceleration = maxMoveSpeed * -1;
+		axisH = Input.GetAxis("Horizontal");
+		//Test if trying to move towards wall and stop movement as well as decrease negative y velocity.
+		if (status.onLeftWall && axisH < 0) {
+			if (rigidBody2D.velocity.y < -1) {
+				rigidBody2D.velocity = new Vector2 (0, -1);
+			} else {
+				rigidBody2D.velocity = new Vector2 (0, rigidBody2D.velocity.y);
 			}
-			Vector2 vector2 = rigidBody2D.velocity;
-			vector2.x = moveAcceleration * Math.Abs(Input.GetAxis("Horizontal"));
-			rigidBody2D.velocity = vector2; //Set horizontal movement velocity relative to the current velocity.
+		} else if (status.onRightWall && axisH > 0) {
+			if (rigidBody2D.velocity.y < -1) {
+				rigidBody2D.velocity = new Vector2 (0, -1);
+			} else {
+				rigidBody2D.velocity = new Vector2 (0, rigidBody2D.velocity.y);
+			}
 		}
-		if (rigidBody2D.velocity.y < -1 && status.onRightWall && Input.GetAxis ("Horizontal") > 0) {
-			rigidBody2D.velocity = new Vector2 (rigidBody2D.velocity.x, -1);
+		//Movement for when in air.
+		else if (!status.onGround) {
+			if (Mathf.Abs (rigidBody2D.velocity.x) < maxMoveSpeed) {
+				rigidBody2D.AddForce (Vector2.right * axisH * airSpeed);
+			} else if (Mathf.Sign (axisH) != Mathf.Sign (rigidBody2D.velocity.x)) {
+				rigidBody2D.AddForce (Vector2.right * axisH * airSpeed);
+			}
 		}
-		if (rigidBody2D.velocity.y < -1 && status.onLeftWall && Input.GetAxis ("Horizontal") < 0) {
-			rigidBody2D.velocity = new Vector2 (rigidBody2D.velocity.x, -1);
+		//Movement
+		else if (Mathf.Abs (rigidBody2D.velocity.x) < maxMoveSpeed) {
+			rigidBody2D.AddForce (Vector2.right * axisH * moveSpeed);
+		} else if (Mathf.Sign (axisH) != Mathf.Sign (rigidBody2D.velocity.x)) {
+			rigidBody2D.AddForce (Vector2.right * axisH * moveSpeed);
+		}
+		//Decrease velocity when not trying to move.
+		if (axisH == 0 && status.onGround) {
+			if (rigidBody2D.velocity.x > maxMoveSpeed / 3) {
+				rigidBody2D.AddForce (Vector2.left * moveSpeed);
+			}
+			else if (rigidBody2D.velocity.x < maxMoveSpeed / -3) {
+				rigidBody2D.AddForce (Vector2.right * moveSpeed);
+			}
 		}
 	}
 }

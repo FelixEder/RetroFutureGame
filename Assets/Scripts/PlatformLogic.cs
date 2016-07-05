3d6a86be9e7e7567b900e501717e0195a58c5eff
felixed <felixed@kth.se>
2016-07-05 15:48:48 +0200

    Added ability to Jump down through platforms in charJump and rewrote platformLogic to use a method.

--- Assets/Scripts/PlatformLogic.cs
+++ Assets/Scripts/PlatformLogic.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 using System.Collections;
 
 public class PlatformLogic : MonoBehaviour {
-	void Update() {
-	}
+	public bool jumpDown;
 
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.gameObject.name == "platformTrigger") {
-			Debug.Log ("collision enter");
-			Physics2D.IgnoreCollision (GetComponent<Collider2D>(), GameObject.Find("char").GetComponent<Collider2D>(), true);
+			IgnorePlatformCollision (true);
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D col) {
 		if (col.gameObject.name == "platformTrigger") {
-			Debug.Log ("collision exit");
-			Physics2D.IgnoreCollision (GetComponent<Collider2D>(), GameObject.Find("char").GetComponent<Collider2D>(), false);
+			IgnorePlatformCollision (false);
 		}
+	}
+
+	public void IgnorePlatformCollision(bool input) {
+		Physics2D.IgnoreCollision (GetComponent<Collider2D> (), GameObject.Find ("char").GetComponent<Collider2D> (), input);
 	}
 }
 

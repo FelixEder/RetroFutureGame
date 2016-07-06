5f1cdc00c513f6792b603a1c8d0b7410a74ac2cf
aktersnurra95 <aktersnurra95@gmail.com>
2016-07-06 14:34:05 +0200

    Fixed issue with the doors not returning when invisible for the camera

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 using System.Collections;
 
 public class Door : MonoBehaviour {
+	public Sprite open, close;
+	void Start() {
+		
+	}
 
 	public void setInvisible() {
-		GetComponent<Renderer>().enabled = false;
+		GetComponent<SpriteRenderer> ().sprite = open;
 		GetComponent<Collider2D> ().isTrigger = true;
 	}
 
 	void OnBecameInvisible() {
-		GetComponent<Renderer>().enabled = true;
+		GetComponent<SpriteRenderer> ().sprite = close;
 		GetComponent<Collider2D> ().isTrigger = false;
 	}
 }

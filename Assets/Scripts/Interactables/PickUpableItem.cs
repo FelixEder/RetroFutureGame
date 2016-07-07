3ed2556c5657edc2c7d9d01db9581caf03efb4e0
Felix Eder <felix.eder@mail.com>
2016-07-07 15:56:38 +0200

    Small fixes on the rock

--- Assets/Scripts/Interactables/PickUpableItem.cs
+++ Assets/Scripts/Interactables/PickUpableItem.cs
@@ -14,6 +14,7 @@ public class PickUpableItem : MonoBehaviour {
 
 	public void PickedUp(GameObject player) {
 		this.gameObject.transform.SetParent (player.transform);
+		this.gameObject.GetComponent<Rigidbody2D> ().isKinematic = true;
 		beingHeld = true;
 	}
 }

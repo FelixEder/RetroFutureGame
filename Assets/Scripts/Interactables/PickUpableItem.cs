f36fcfa5b6c872ccda5fdddfe6b98bd585a32451
Felix Eder <felix.eder@mail.com>
2016-07-07 15:47:32 +0200

    Added new scripts

---
+++ Assets/Scripts/Interactables/PickUpableItem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpableItem : MonoBehaviour {
+	public float HoldPositionX = 0.341f, HoldPositionY = -0.332f;
+	bool beingHeld;
+
+
+	void FixedUpdate() {
+		if (beingHeld) {
+			this.gameObject.transform.localPosition = new Vector2 (HoldPositionX, HoldPositionY);
+		}
+	}
+
+	public void PickedUp(GameObject player) {
+		this.gameObject.transform.SetParent (player.transform);
+		beingHeld = true;
+	}
+}

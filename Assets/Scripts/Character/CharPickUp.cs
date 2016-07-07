267b78c97e9a172c517142dbff7753c7e075e8e6
Felix Eder <felix.eder@mail.com>
2016-07-07 13:27:29 +0200

    Added missing scripts

---
+++ Assets/Scripts/Character/CharPickUp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharPickUp : MonoBehaviour {
+	CharInventory charInventory;
+
+	void Start() {
+		charInventory = GetComponent<CharInventory> ();
+	}
+
+	void FixedUpdate() {
+		if (Input.GetKey (KeyCode.L) && charInventory.isHoldingItem ()) {
+			transform.parent = null;
+			charInventory.setHoldingItem (null);
+		}
+	}
+
+
+	void OnCollisionStay2D(Collision2D col) {
+		switch(col.gameObject.tag) {
+
+		case "rock" :
+			if(Input.GetKey(KeyCode.L) && !charInventory.isHoldingItem()) {
+				charInventory.setHoldingItem(col.gameObject); 
+				col.gameObject.transform.parent = transform;
+			}
+			break;
+		}
+	}
+}

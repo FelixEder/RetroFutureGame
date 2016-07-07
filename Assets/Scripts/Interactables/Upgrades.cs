00899356e9fed26660aa40b5994c55c0c1d3cde2
Felix Eder <felix.eder@mail.com>
2016-07-08 01:56:27 +0200

    Leaf-upgrade now works really well

--- Assets/Scripts/Interactables/Upgrades.cs
+++ Assets/Scripts/Interactables/Upgrades.cs
@@ -4,23 +4,15 @@ using System.Collections;
 public class Upgrades : MonoBehaviour {
 	//Name has to be the same 
 	public string type;
-	CharInventory charInventory;
-
-	void Start() {
-		charInventory = GetComponent<CharInventory> ();
-	}
 
 	void OnTriggerEnter2D(Collider2D col) {
-		if (!col.isTrigger) {
-			//In the future maybe a check so that only the player can activate the item?
-
+		if (!col.isTrigger && col.gameObject.tag.Equals("char")) {
 			//Play correct music and animation depending on what switch-option is chosen, change player-image somewhat and display a tutorial for the item.
-			switch (name) {
+			switch (type) {
 
 			case "leaf":
-				charInventory.leaf = true;
+				col.gameObject.GetComponent<CharInventory>().leaf = true;
 				break;
-
 			}
 			Destroy (gameObject);
 		}

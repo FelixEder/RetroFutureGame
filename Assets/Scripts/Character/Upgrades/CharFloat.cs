8bc09d28afa3458d4df41e7f7e379d92ccdfff7c
Felix Eder <felix.eder@mail.com>
2016-07-10 09:57:08 +0200

    Added missing CharFloat.cs to git

---
+++ Assets/Scripts/Character/Upgrades/CharFloat.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharFloat : MonoBehaviour {
+	Rigidbody2D rigidBody2D;
+	CharJump charJump;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody2D = GetComponent<Rigidbody2D> ();
+		charJump = GetComponent<CharJump> ();
+	}
+
+	void FixedUpdate () {
+		//This could later be changed to so that when an upgrade is obtained, this part of the script is enabled for the player.
+		if (charJump.hasJumped && rigidBody2D.velocity.y <= -1f && Input.GetButton ("Leaf")) {
+			Debug.Log ("Upgrade tried!");
+			rigidBody2D.velocity = new Vector2 (rigidBody2D.velocity.x, -1f);
+		}
+	}
+}

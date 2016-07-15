742d6820f50ff839327b422db0c2b385bf050fa5
Felix Eder <felix.eder@mail.com>
2016-07-15 18:23:32 +0200

    Fixed healthslider script and gameobject, adaptive to max health and current health.

--- Assets/Scripts/Character/CharHealth.cs
+++ Assets/Scripts/Character/CharHealth.cs
@@ -3,9 +3,14 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class CharHealth : MonoBehaviour {
-	public int currentHealth, maxHealth;
+	public float currentHealth, maxHealth;
+
+	void Update() {
+		SetHealthSlider ();
+	}
 
 	void SetHealthSlider() {
-		GameObject.Find ("healthSlider").Slider.Set (currentHealth / maxHealth);
+		GameObject.Find ("healthSlider").GetComponent<Slider>().value = currentHealth / maxHealth;
+		GameObject.Find ("healthSlider").GetComponent<RectTransform> ().sizeDelta = new Vector2 (8 + 32 * maxHealth, 32);
 	}
 }

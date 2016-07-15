4476b71e32622513d3a5b4c65df0b792265bccb3
Felix Eder <felix.eder@mail.com>
2016-07-15 19:47:37 +0200

    Continued work on the CharStomp

--- Assets/Scripts/Character/Upgrades/CharStomp.cs
+++ Assets/Scripts/Character/Upgrades/CharStomp.cs
@@ -5,22 +5,29 @@ public class CharStomp : MonoBehaviour {
 	Rigidbody2D rigidBody2D;
 	CharStatus charStatus;
 	public bool isStomping;
+	bool holdStomp;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody2D = GetComponent<Rigidbody2D> ();
 		charStatus = GetComponent<CharStatus> ();
-		isStomping;
 		//Change sprite, display correct tutorial and play theme.
+	}
+
+	void Update() {
+		if (!Input.GetButton ("Attack"))
+			holdStomp = false;
 	}
 	
 	void FixedUpdate() {
-		if (charStatus.InAir () && (Input.GetAxis ("Vertical") < -0.7) && Input.GetButton ("Attack")) {
+		if (charStatus.InAir () && Input.GetAxis ("Vertical") < -0.7 && Input.GetButton ("Attack") && !holdStomp) {
+			holdStomp = true;
 			isStomping = true;
 			rigidBody2D.velocity = new Vector2 (0, 0);
+			rigidBody2D.gravityScale = 0.0f;
 			//Play stomp-animation
-			Invoke ("Stomp", 1f);
-		} else if (charStatus.OnGround) {
+			Invoke ("Stomp", 0.5f);
+		} else if (charStatus.onGround) {
 			//Play stomp-ground animation
 			Invoke ("FinishedStomp", 1f);
 		}
@@ -31,7 +38,8 @@ public class CharStomp : MonoBehaviour {
 	}
 
 	void Stomp() {
-		rigidBody2D.velocity = new Vector2 (0, 9f);
+		rigidBody2D.gravityScale = 2.0f;
+		rigidBody2D.velocity = new Vector2 (0, -9f);
 	}	
 }
 

ae8fb0709d5fdee2cf9eb062c4c7f639e6b7bb6f
Sendan <king-viktor@hotmail.com>
2016-07-16 19:45:22 +0200

    Added dangerous blue jumping critter

---
+++ Assets/Scripts/Enemies/JumpingCritter.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using System.Collections;
+
+public class JumpingCritter : MonoBehaviour {
+	public float moveSpeed, knockForce, jumpSpeed;
+	bool isMirrored = false;
+	Rigidbody2D rb2D;
+	public int health = 2, damage = 1;
+
+	void Start() {
+		rb2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate() {
+		if (isMirrored) {
+			rb2D.velocity = new Vector2 (moveSpeed, rb2D.velocity.y);
+		} else {
+			rb2D.velocity = new Vector2 (-1 * moveSpeed, rb2D.velocity.y);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+
+		switch(col.gameObject.tag) {
+
+			case "char":
+				col.gameObject.GetComponent<CharHealth> ().TakeDamage (damage);
+				col.gameObject.GetComponent<Knockback>().Knock(this.gameObject, knockForce);
+				break;
+
+			case "softEnemy" :
+				GetMirrored();
+				break;
+
+			case "wall" :
+				GetMirrored();
+				break;
+
+			case "door" :
+				GetMirrored();
+				break;
+
+			case "rock":
+				if (col.gameObject.GetComponent<Rigidbody2D> ().velocity.magnitude >= 3.0f) {
+					GetHurt (col.gameObject.GetComponent<PickUpableItem> ().damage);
+				}
+				GetMirrored ();
+				break;
+
+			case "branch":
+				if (col.gameObject.GetComponent<Rigidbody2D> ().velocity.magnitude >= 2.0f) {
+					GetHurt (col.gameObject.GetComponent<PickUpableItem> ().damage);
+				}
+				GetMirrored ();
+				break;
+
+		}
+	}
+
+	/**
+	 * Mirrors the enemy and therefor makes it change direction.
+	 */
+	void GetMirrored() {
+		if(!isMirrored) {
+			transform.rotation = Quaternion.Euler(0, 180, 0);
+			isMirrored = true;
+		}
+		else {
+			transform.rotation = Quaternion.Euler(0, 0, 0);
+			isMirrored = false;
+		}
+	}
+
+	/**
+	 * Method called when enemy is hit by the player
+	 */
+	public void GetHurt(int damage) {
+		//Play a sound and animation.
+		health -= damage;
+		if (health <= 0) {
+			//Enemy is dead, play animation and sound.
+			if (Random.Range (0, 50) < 25) {
+				Instantiate (Resources.Load ("HealthDrop"), transform.position, Quaternion.identity);
+			}
+			Destroy (this.gameObject);
+		}
+		GetMirrored ();
+	}
+
+	public void Jump () {
+		if (Random.Range (0, 100) < 5) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpSpeed);
+		}
+	}
+}

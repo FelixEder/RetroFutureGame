c1e1fbfef3744f2b92201d8d3975a1bfa7000182
aktersnurra95 <aktersnurra95@gmail.com>
2016-07-24 01:38:48 +0200

    removed and readded enemy knockback to fix an issue where file was being renamed with a big B for knockback

---
+++ Assets/Scripts/Enemies/EnemyKnockback.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyKnockback : MonoBehaviour {
+	float wayofKnock;
+	
+	public void Knock(GameObject attacker, float force) {
+		if (this.gameObject.transform.position.x <= attacker.transform.position.x)
+			wayofKnock = -1;
+		else {
+			wayofKnock = 1;
+		}
+		this.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (force * wayofKnock, 2), ForceMode2D.Impulse);
+	}
+}
+

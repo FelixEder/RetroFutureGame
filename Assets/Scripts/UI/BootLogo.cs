9247d0275652cc27f1c0dbe107243a40f71d43b0
aktersnurra95 <aktersnurra95@gmail.com>
2016-07-24 23:47:01 +0200

    Made boot functionable again, recentered main menu buttons, added earlygame load button to main menu.

---
+++ Assets/Scripts/UI/BootLogo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class BootLogo : MonoBehaviour {
+	void Start() {
+		StartCoroutine (TransitionImage ());
+	}
+	IEnumerator TransitionImage() {
+		GetComponent<Image>().CrossFadeAlpha(0, 0, false);
+		yield return new WaitForSeconds(1);
+		GetComponent<Image>().CrossFadeAlpha(1, 2, false);
+		yield return new WaitForSeconds(8);
+		GetComponent<Image>().CrossFadeAlpha(0, 2, false);
+		yield return new WaitForSeconds(4);
+		SceneManager.LoadScene ("MainMenu");
+	}
+}

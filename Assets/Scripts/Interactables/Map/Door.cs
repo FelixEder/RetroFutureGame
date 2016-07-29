a7652d7d86348370f9e35935b4e566a6a79eb456
aktersnurra95 <aktersnurra95@gmail.com>
2016-07-29 23:16:24 +0200

    Added new smooth camera movement and dynamic positioning of the camera view, temporarily removed door-cover code

--- Assets/Scripts/Interactables/Map/Door.cs
+++ Assets/Scripts/Interactables/Map/Door.cs
@@ -3,28 +3,28 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 	public Sprite open, close;
-	public GameObject cover1, cover2;
+//	public GameObject cover1, cover2;
 	Color cover1Color, cover2Color;
 	public int health;
 
 	void Start() {
-		cover1Color = cover1.GetComponent<SpriteRenderer> ().color;
-		cover2Color = cover2.GetComponent<SpriteRenderer> ().color;
+//		cover1Color = cover1.GetComponent<SpriteRenderer> ().color;
+//		cover2Color = cover2.GetComponent<SpriteRenderer> ().color;
 	}
 
 	public void SetInvisible() {
 		GetComponent<SpriteRenderer> ().sprite = open;
 		GetComponent<Collider2D> ().isTrigger = true;
-		StartCoroutine(UncoverMap ());
+//		StartCoroutine(UncoverMap ());
 	}
 
 	void OnBecameInvisible() {
 		GetComponent<SpriteRenderer> ().sprite = close;
 		GetComponent<Collider2D> ().isTrigger = false;
-		CoverMap ();
+//		CoverMap ();
 	}
 
-	IEnumerator UncoverMap() {
+/*	IEnumerator UncoverMap() {
 		for (float a = 1; a > 0; a -= 0.05f) {
 			if (cover1 != null && cover1Color.a > 0)
 				cover1.GetComponent<SpriteRenderer> ().color = new Color (cover1Color.r, cover1Color.g, cover1Color.b, a);
@@ -44,4 +44,5 @@ public class Door : MonoBehaviour {
 		if (cover2 != null && !cover2.GetComponent<MapCover> ().isInside)
 			cover2.GetComponent<SpriteRenderer> ().color = new Color (cover2Color.r, cover2Color.g, cover2Color.b, 1);
 	}
+*/
 }

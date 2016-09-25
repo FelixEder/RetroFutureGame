24b069d7ff5592dd6efb3706442bd454b8d58861
Felix Eder <felix.eder@mail.com>
2016-09-25 14:19:34 +0200

    The Itemprefabspawner now spawns items randomly within a set area

--- Assets/Scripts/Interactables/Misc/PickupableItemSpawner.cs
+++ Assets/Scripts/Interactables/Misc/PickupableItemSpawner.cs
@@ -8,20 +8,23 @@ public class PickupableItemSpawner : MonoBehaviour {
 	/**The amount of time in seconds between each spawn*/
 	public float spawnStart, spawnInterval;
 	/**The max amount of simultaneously spawned prefabs.*/
-	public int maxSimultaneous;
+	public int maxSimultaneous, spawnArea;
 	//The current number of spawned prefabs.
 	int current;
 	/**The name of the prefab to spawn*/
 	public string prefab;
+	Vector3 spawnLoc;
 
 	void Start() {
 		InvokeRepeating ("Spawn", spawnStart, spawnInterval);
+		spawnLoc = transform.position;
 	}
 
 	/**Instantiates "type" prefab as gameobject and sets this gameobject as parent if the parent's childcount is less than maxSimultaneous.*/
 	void Spawn() {
 		if (current < maxSimultaneous) {
-			GameObject instance = Instantiate (Resources.Load (prefab), transform.position, Quaternion.identity) as GameObject;
+			Vector3 babyBoom = new Vector3 (spawnLoc.x + Random.Range (-spawnArea, spawnArea), spawnLoc.y + Random.Range (-spawnArea, spawnArea), spawnLoc.z);
+			GameObject instance = Instantiate (Resources.Load (prefab), babyBoom, Quaternion.identity) as GameObject;
 			instance.GetComponent<PickUpableItem> ().PIS = GetComponent<PickupableItemSpawner> ();
 			IncreaseCurrent ();
 		}

b8f33af78f4f1011415429ffc2dfb241cf68cc13
Felix Eder <felix.eder@mail.com>
2016-10-18 17:40:15 +0200

    Wrote the script and implemented the countdown in unity, works as it should

---
+++ Assets/Scripts/Events/CountDown.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CountDown : MonoBehaviour {
+	int timeLeft = 300;
+	public Text text;
+
+	void Start () {
+		text.enabled = true;
+		InvokeRepeating ("Counting", 5, 1);
+	}
+
+	void Counting() {
+		text.alignment = TextAnchor.MiddleCenter;
+		timeLeft--;
+		text.text = timeLeft.ToString();
+		if (timeLeft <= 0) {
+		//Load game-over screen
+		}
+	}
+}

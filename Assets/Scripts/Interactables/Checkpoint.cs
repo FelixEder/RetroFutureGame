9cc6a0f6b3fa8058a5f6d6dbf0bfe19e719952e3
Felix Eder <felix.eder@mail.com>
2016-10-19 15:51:41 +0200

    Created the basic checkpoint script as well as unity object

---
+++ Assets/Scripts/Interactables/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+	public GameObject checkPoint;
+
+	void Start () {
+		//Set the starting location of the game to current checkpoint here.
+	}
+	
+	void OnTriggerEnter2D(Collider2D pointcheck) {
+		if(pointcheck.gameObject.tag.Equals("CheckPoint")) {
+			SetCheckPoint(pointcheck.gameObject);
+		}
+	}
+
+	void SetCheckPoint(GameObject location) {
+		checkPoint = location;
+		//Maybe do some nice animation and add sound.
+		Debug.Log("A location has been set!");
+	}
+}

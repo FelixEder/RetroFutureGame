6bc473edba75898916e72b429e8bfb98e580a613
aktersnurra95 <aktersnurra95@gmail.com>
2017-02-16 21:39:54 +0100

    Made pickup trigger function primarily the same way as punch trigger but is only enabled for one frame and can only pickup one item.

--- Assets/Scripts/Character/Actions/CharPickUp.cs
+++ Assets/Scripts/Character/Actions/CharPickUp.cs
@@ -5,7 +5,7 @@ public class CharPickUp : MonoBehaviour {
 	CharStatus status;
 	CharInventory inventory;
 	InputManager input;
-	bool holdPickup;
+	bool holdPickup, cooldown;
 
 	void Start() {
 		inventory = transform.parent.GetComponent<CharInventory> ();
@@ -14,6 +14,11 @@ public class CharPickUp : MonoBehaviour {
 	}
 
 	void Update() {
+		if (GetComponent<BoxCollider2D> ().enabled)
+			GetComponent<BoxCollider2D> ().enabled = false;
+		if (cooldown)
+			cooldown = false;
+		
 		if (!input.GetKey("grab") && holdPickup) {
 			holdPickup = false;
 		}
@@ -22,20 +27,23 @@ public class CharPickUp : MonoBehaviour {
 			inventory.GetHoldingItem ().GetComponent<PickUpableItem> ().Drop (true);
 			inventory.SetHoldingItem (null);
 		}
+		else if(input.GetKey ("grab") && !holdPickup && !inventory.IsHoldingItem () && !status.isSmall) {
+			holdPickup = true;
+			GetComponent<BoxCollider2D> ().enabled = true;
+		}
 	}
 		
-	void OnTriggerStay2D(Collider2D col) {
-		if (input.GetKey ("grab") && !holdPickup && !inventory.IsHoldingItem () && !status.isSmall) {
-			holdPickup = true;
+	void OnTriggerEnter2D(Collider2D col) {
+		if (!cooldown) {
 			Debug.Log ("Tried to pick up " + col.gameObject);
-			switch (col.gameObject.GetComponent<PickUpableItem>().GetItemType()) {
-
-				case "Rock":
-				case "Branch":
-					inventory.SetHoldingItem (col.gameObject);
-					col.gameObject.GetComponent<PickUpableItem> ().PickUp (this.gameObject);
-					break;
+			switch (col.gameObject.GetComponent<PickUpableItem> ().GetItemType ()) {
+			case "Rock":
+			case "Branch":
+				inventory.SetHoldingItem (col.gameObject);
+				col.gameObject.GetComponent<PickUpableItem> ().PickUp (this.gameObject);
+				break;
 			}
 		}
+		cooldown = true;
 	}
 }

6e483406fd4f37b86eadf2d454f432d335071d68
aktersnurra95 <aktersnurra95@gmail.com>
2017-02-17 00:44:09 +0100

    Added new spritesheet for electrohaz and converted script to use animation instead of sprites. Added new sprite for spikesfromGoogle.

--- Assets/Scripts/Enviroment/Obstacles/Electrohaz.cs
+++ Assets/Scripts/Enviroment/Obstacles/Electrohaz.cs
@@ -2,17 +2,41 @@ using UnityEngine;
 using System.Collections;
 
 public class Electrohaz : MonoBehaviour {
-	bool isActive;
+	/* NOT USED
 	public Sprite inactive, charging, active;
 	SpriteRenderer sr;
-	public int damage = 99;
+	*/
+	bool isActive;
+	public int damage;
 
 	void Start() {
+		/* NOT USED
 		sr = GetComponent<SpriteRenderer>();
 		sr.sprite = inactive;
 		InvokeRepeating("EspisMetod", 0, 2);
+		*/
+		StartCoroutine (GustavsMetod ());
 	}
 
+	void Update() {
+		transform.localPosition = new Vector2 (transform.localPosition.x + 0.1f, transform.localPosition.y);
+		transform.localPosition = new Vector2 (transform.localPosition.x - 0.1f, transform.localPosition.y);
+	}
+
+	IEnumerator GustavsMetod() {
+		while (true) {
+			yield return new WaitForSeconds (2f);
+			GetComponent<Animator> ().SetInteger ("state", 1);
+			yield return new WaitForSeconds (GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).length * 2);
+			GetComponent<Animator> ().SetInteger ("state", 2);
+			isActive = true;
+			yield return new WaitForSeconds (GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).length * 2);
+			GetComponent<Animator> ().SetInteger ("state", 0);
+			isActive = false;
+		}
+	}
+
+	/* NOT USED
 	void EspisMetod() {
 		//Debug.Log (sr.sprite + " " + inactive + charging + active);
 		if(sr.sprite == inactive) {
@@ -28,9 +52,10 @@ public class Electrohaz : MonoBehaviour {
 			isActive = false;
 		}
 	}
+	*/
+
 	void OnTriggerStay2D(Collider2D col) {
 		switch(col.gameObject.tag) {
-
 		case "Char":
 			if (isActive) {
 				col.gameObject.GetComponent<CharHealth> ().TakeDamage (damage);
@@ -48,7 +73,6 @@ public class Electrohaz : MonoBehaviour {
 		case "Door" :
 		case "PickupableItem":
 			break;
-
 		}
 	}
 }

265076569c46bb08a593e28ac4277cba51f51e5d
aktersnurra95 <aktersnurra95@gmail.com>
2018-01-23 19:38:44 +0100

    Converted camera system to use cinemachine. Started converting to use Tilemaps for leveldesign.

---
+++ Assets/Scripts/Other/CameraParallax.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraParallax : MonoBehaviour {
+	public Camera backCamera, frontCamera;
+	public float backOffset, backMultiplier, frontOffset, frontMultiplier;
+	float ortSize;
+
+	void Start () {
+		
+	}
+
+	void LateUpdate() {
+		ortSize = GetComponent<Camera>().orthographicSize;
+
+		backCamera.fieldOfView = backOffset + ortSize * backMultiplier;
+		frontCamera.fieldOfView = frontOffset + ortSize * frontMultiplier;
+	}
+}

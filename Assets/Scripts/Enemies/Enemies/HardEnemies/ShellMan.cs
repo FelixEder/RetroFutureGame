d54f26812eec089c99054a07d1b327add0d8f735
Felix Eder <felix.eder@mail.com>
2018-06-08 22:14:28 +0200

    Re-added missing shellmnan script

---
+++ Assets/Scripts/Enemies/Enemies/HardEnemies/ShellMan.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class ShellMan : MonoBehaviour {
+	private bool deShelled;
+	private Animator anim;
+	public int newDamage;
+	private float newKnockbackForce;
+	public FollowRange newFollowRange;
+	
+	void start() {
+		anim = GetComponent<Animator>();
+	}
+
+	public void BreakShield() {
+		anim.SetTrigger("next");
+		deShelled = true;
+
+		var attack = GetComponent<EnemyAttack>();
+		attack.damage = newDamage;
+		attack.knockbackForce = newKnockbackForce;
+
+		var moment = GetComponent<EnemyMovement>();
+		moment.followRange = newFollowRange;
+		var jump = GetComponent<EnemyJump>();
+		jump.enabled = true;
+	}
+}

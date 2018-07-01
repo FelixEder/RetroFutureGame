31c56e4dcc651a0a93e1b5c42aa8fe3a7fbbe7bf
aktersnurra95 <aktersnurra95@gmail.com>
2018-07-02 01:06:44 +0800

    Remade entire Tutorial system to use dynamic in world prompts

---
+++ Assets/Scripts/UI/TutorialMover.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMover : MonoBehaviour {
+    public GameObject player;
+    RelativeJoint2D target;
+	GameObject current;
+    Vector2 direction;
+
+    // Use this for initialization
+    void Start() {
+        target = GetComponent<RelativeJoint2D>();
+		DisableAll();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+		if(target.connectedBody == null)
+			target.connectedBody = player.GetComponent<Rigidbody2D>();
+        if(target.connectedBody.gameObject != player) {
+            direction = target.connectedBody.transform.position - player.transform.position;
+            direction.Normalize();
+            target.linearOffset = direction;
+        }
+		else
+			target.linearOffset = Vector2.right;
+    }
+	
+	public void SetCurrent(GameObject tutorial, Rigidbody2D _target) {
+		if(current != null)
+			DisableCurrent();
+		Enable(tutorial, _target);
+	}
+	
+	public void ChangeTarget(Rigidbody2D _target) {
+		target.connectedBody = _target;	
+	}
+	
+	public void DisableAll() {
+		foreach(Transform child in transform)
+			child.gameObject.SetActive(false);
+		target.connectedBody = null;
+	}
+	
+	public void DisableCurrent() {
+		current.SetActive(false);
+		target.connectedBody = null;
+		current = null;
+	}
+	
+	void Enable(GameObject tutorial, Rigidbody2D _target) {
+		tutorial.SetActive(true);
+		target.connectedBody = _target;
+		current = tutorial;
+	}
+}
--- Assets/Scripts/UI/TutorialTrigger.cs
+++ Assets/Scripts/UI/TutorialTrigger.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class TutorialTrigger : MonoBehaviour {
+	
 	public float timeToDisplay;
 	public bool stringIsHideKey;
-	public string tutorial;
+	public string tutorialString;
 	[TextArea]
 	public string tutorialText;
 
 	TutorialManager manager;
-
+	
+	/*
 	void Start() {
 		manager = GameObject.Find("tutorialPanel").GetComponent<TutorialManager>();
 	}
@@ -20,5 +22,21 @@ public class TutorialTrigger : MonoBehaviour {
 			manager.DisplayTutorial(tutorial, tutorialText, stringIsHideKey, timeToDisplay);
 			Destroy(gameObject);
 		}
+	}
+	*/
+	public TutorialMover tutorialMover;
+	public GameObject tutorial;
+	public Rigidbody2D target;
+	
+	
+	
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.gameObject.tag.Equals("Player"))
+			tutorialMover.SetCurrent(tutorial, target);
+	}
+	
+	void OnTriggerExit2D(Collider2D col) {
+		if(col.gameObject.tag.Equals("Player"))
+			tutorialMover.ChangeTarget(col.GetComponent<Rigidbody2D>());
 	}
 }
